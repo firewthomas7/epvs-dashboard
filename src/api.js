74933c90f8b07d2daabb2be936c49f8808e913c2
firewthomas7 <firewthomas7@gmail.com>
2026-05-23 00:22:35 +0300

    fix: update axios baseURL to live backend production URL

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://onrender.com',
+  baseURL: 'https://epvs-backend.onrender.com',
   headers: { 'Content-Type': 'application/json' },
 })
 
